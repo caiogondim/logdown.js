8f8140074d727983497eeee5a67497014c40c68b
Caio Gondim <me@caiogondim.com>
2015-02-15 02:09:57 +0100

    Test for different prefix color for each instance

--- test/index.js
+++ test/index.js
@@ -20,6 +20,16 @@ describe('new Logdown()', function() {
     var foo2 = new Logdown({prefix: 'foo'})
     assert.equal(foo, foo2)
   })
+
+  it('should give a new prefix color for each instance', function() {
+    var foo = new Logdown({prefix: 'foo'})
+    var bar = new Logdown({prefix: 'bar'})
+    var quz = new Logdown({prefix: 'quz'})
+    var baz = new Logdown({prefix: 'baz'})
+    assert.notEqual(foo.prefixColor, bar.prefixColor)
+    assert.notEqual(foo.prefixColor, quz.prefixColor)
+    assert.notEqual(foo.prefixColor, baz.prefixColor)
+  })
 })
 
 describe('Logdown.enable', function() {
