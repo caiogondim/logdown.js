c24d9d3f66981e438e39d3a7f7039e79689d1c55
Caio Gondim <me@caiogondim.com>
2016-06-29 17:53:35 +0200

    1.2.6

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 /**
  * logdown - Debug utility with markdown support that runs on browser and server
  *
- * @version v1.2.5
+ * @version v1.2.6
  * @link https://github.com/caiogondim/logdown
  * @author Caio Gondim <me@caiogondim.com> (http://caiogondim.com)
  * @license ISC
