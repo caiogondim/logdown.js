b821f60237197132c34795a9497176dd38aa5f1e
Benny Neugebauer <bn@bennyn.de>
2016-09-30 12:25:04 +0200

    Sorted by A-Z

--- dist/logdown.d.ts
+++ dist/logdown.d.ts
@@ -1,10 +1,10 @@
 declare class Logdown {
   constructor(options: Object);
-  public log(...args: string[]): void;
-  public info(...args: string[]): void;
-  public warn(...args: string[]): void;
   public debug(...args: string[]): void;
   public error(...args: string[]): void;
+  public info(...args: string[]): void;
+  public log(...args: string[]): void;
+  public warn(...args: string[]): void;
   public static disable(...args: string[]): void;
   public static enable(...args: string[]): void;
 }
