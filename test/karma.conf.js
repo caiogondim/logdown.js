561be959c50ac5756fad1b9ecf4b03596b1ccfb3
Caio Gondim <me@caiogondim.com>
2016-10-28 11:57:26 -0400

    Make tests portable between macOS and Windows
    Removing Safari from Karma since there is no Safari browser on Windows

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-
     // launcher
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: ['Chrome', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
