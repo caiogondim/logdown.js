3ec7719289d1b645867d96adcca6f5353edda6a8
Benny Neugebauer <bn@bennyn.de>
2017-01-20 00:17:07 +0100

    Added sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var header = require('gulp-header')
 var karma = require('karma').server
 var mocha = require('gulp-mocha')
 var rename = require('gulp-rename')
+var sourcemaps = require('gulp-sourcemaps')
 var uglify = require('gulp-uglify')
 
 // Test
@@ -59,10 +60,12 @@ gulp.task('build', function() {
   gulp.src('src/logdown.js')
     .pipe(header(banner, {pkg: pkg}))
     .pipe(gulp.dest('dist'))
+    .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(header(banner, {pkg: pkg}))
     .pipe(gulp.dest('./example/lib'))
     .pipe(rename({extname: '.min.js'}))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 })
 
