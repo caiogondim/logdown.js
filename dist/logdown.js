a2453a1efeece6bdb95b7369ca79894f533cd44e
Caio Gondim <me@caiogondim.com>
2017-07-13 15:24:32 -0400

    3.0.1

--- dist/logdown.js
+++ dist/logdown.js
@@ -313,16 +313,17 @@ module.exports = [
 },{}],6:[function(require,module,exports){
 (function (global){
 /* eslint-disable no-new-func */
+/* global self, global */
 
-module.exports = function getGlobal() {
-    // return global object based on the environment present in.
-    // window for browser and global for node.
-    // https://github.com/purposeindustries/window-or-global/blob/master/lib/index.js
-    return (
+module.exports = function getGlobal () {
+  // Return the global object based on the environment presently in.
+  // window for browser and global for node.
+  // Ref from -> https://github.com/purposeindustries/window-or-global/blob/master/lib/index.js
+  return (
         (typeof self === 'object' && self.self === self && self) ||
         (typeof global === 'object' && global.global === global && global) ||
         this
-    )
+  )
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
