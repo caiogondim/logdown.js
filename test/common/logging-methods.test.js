8afcc90e42f755b1f44638f249f91caa02aa1e33
Caio Gondim <me@caiogondim.com>
2017-10-10 13:56:53 -0400

    Fix issue with non iterable console methods
    Issue https://github.com/caiogondim/logdown.js/issues/95

--- test/common/logging-methods.test.js
+++ test/common/logging-methods.test.js
@@ -6,16 +6,34 @@ const logdown = require('../../src/node')
 // Tests
 //
 
+function beforeEach () {
+  logdown._instances = []
+  logdown._setPrefixRegExps()
+}
+
 it('has a facade for every method on opts.logger', () => {
+  beforeEach()
+
   const logger = {
     log: jest.fn(),
     warn: jest.fn(),
     error: 'not a function'
   }
-
   const foo = logdown('foo', { logger: logger })
 
   expect(typeof foo.log).toBe('function')
   expect(typeof foo.warn).toBe('function')
   expect(typeof foo.error).toBe('undefined')
+})
+
+// Issue: https://github.com/caiogondim/logdown.js/issues/95
+it('exposes a basic API for `logger` with no iterable methods', () => {
+  beforeEach()
+
+  const logger = {}
+  const foo = logdown('foo', { logger: logger })
+
+  expect(typeof foo.log).toBe('function')
+  expect(typeof foo.warn).toBe('function')
+  expect(typeof foo.error).toBe('function')
 })
